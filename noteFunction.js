5336d7b3e6df2565bb2a059e02933b79b692f285
kendrick Brown <chefken360@gmail.com>
2022-10-29 09:57:53 -0700

    slow progression

--- noteFunction.js
+++ noteFunction.js
@@ -11,4 +11,25 @@ function createNewNote(body, noteArray) {
             notes: noteArray
         }, null, 2)
     )
+    return note;
 }
+function deleteNote(noteArray, id){
+    let deleteId = parseInt(id);
+    noteArray.splice(deleteId, 1);
+
+    for (i = deleteId; i < noteArray.length; i++){
+        noteArray[i].id = i.toString();
+    }
+
+    fs.writeFileSync(
+        path.join(__dirname, './db.json'),
+        JSON.stringify({
+            notes: noteArray
+        },null, 2)
+    )
+}
+
+module.exports = {
+    createNewNote,
+    deleteNote
+};
