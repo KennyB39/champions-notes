14abf1b73c03a72a475da39442e183fe729c9e90
kendrick Brown <chefken360@gmail.com>
2022-10-30 05:29:36 -0700

    rerouting and debugging

--- apiroutes.js/apiRoutes/noteRoutes.js
+++ apiroutes.js/apiRoutes/noteRoutes.js
@@ -1,1 +1,18 @@
 const router = require('express').Router();
+const {note} = require('../../db.json');
+const {createNewNote, noteDeleteNote} = require('../../lib/noteFunction')
+
+router.get('/notes',(req, res) =>{
+    let saved = notes 
+    res.json(saved);
+})
+router.post('/notes', (req, res) =>{
+    req.body.id = notes.legnth.toString();
+    let note = createNewNote(req.body, notes);
+    res.json(note);
+})
+
+router.delete('/notes/:id', (req,res) => {
+    noteDeleteNote(notes, req.params.id);
+    res.json(notes);
+})
