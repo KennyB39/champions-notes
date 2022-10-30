9b96dca06575d5adda1aa7e882a2d26606b41938
kendrick Brown <chefken360@gmail.com>
2022-10-29 17:14:43 -0700

    routges continued

--- Assets/server.js
+++ Assets/server.js
@@ -1,4 +1,4 @@
-const port =  3001;
+const PORT =  3001;
 
 const express = require('express');
 const app = express(); 
@@ -6,4 +6,18 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
-const apiRoutes = require( '../apiroutes.js')
+const apiRoutes = require( '../apiroutes.js');
+const apiRoutes = require('./htmlRoutes');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () =>{
+    console.log(`API server on port ${PORT}`)
+})
